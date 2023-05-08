eb10634fb0b0015d7615012f15ec6a78a2123c25
Diana Opiyo <dopiyo@gmail.com>
2023-05-08 16:35:48 +0300

    added api

--- src/App.js
+++ src/App.js
@@ -1,32 +1,69 @@
-import React from 'react';
-import './App.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { Container } from 'reactstrap';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
-import Inventory from './components/Inventory';
-import ShopLocator from './components/ShopLocator';
-import AgentManagement from './components/AgentManagement';
-import Invoice from './components/Invoice';
-import Receipt from './components/Receipt';
-import Dashboard from './components/Dashboard';
-import Home from './components/Home'; // Import the Home component
+function App() {
+  const [dashboard, setDashboard] = useState({});
+  const [products, setProducts] = useState([]);
 
-const App = () => {
+  useEffect(() => {
+    axios.get('http://localhost:5005/api/dashboard')
+      .then(res => setDashboard(res.data))
+      .catch(err => console.log(err));
+
+    axios.get('http://localhost:5005/api/products')
+      .then(res => setProducts(res.data))
+      .catch(err => console.log(err));
+  }, []);
+
   return (
-    <Router>
-      <Container>
-        <Routes>
-          <Route exact path="/" element={<Home />} /> {/* Add this new Route element */}
-          <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/inventory" element={Inventory} />
-          <Route path="/shop-locator" element={ShopLocator} />
-          <Route path="/agent-management" element={AgentManagement} />
-          <Route path="/invoice" element={Invoice} />
-          <Route path="/receipt" element={Receipt} />
-        </Routes>
-      </Container>
-    </Router>
+    <div>
+      <h1>{dashboard.name}</h1>
+      <p>{dashboard.description}</p>
+
+      <ul>
+        {products.map(product => (
+          <li key={product._id}>
+            <h2>{product.name}</h2>
+            <p>{product.description}</p>
+            <p>${product.price}</p>
+            <p>{product.quantity} in stock</p>
+          </li>
+        ))}
+      </ul>
+    </div>
   );
-};
+}
 
 export default App;
+// import React from 'react';
+// import './App.css';
+// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+// import { Container } from 'reactstrap';
+
+// import Inventory from './components/Inventory';
+// import ShopLocator from './components/ShopLocator';
+// import AgentManagement from './components/AgentManagement';
+// import Invoice from './components/Invoice';
+// import Receipt from './components/Receipt';
+// import Dashboard from './components/Dashboard';
+// import Home from './components/Home'; // Import the Home component
+
+// const App = () => {
+//   return (
+//     <Router>
+//       <Container>
+//         <Routes>
+//           <Route exact path="/" element={<Home />} /> {/* Add this new Route element */}
+//           <Route path="/dashboard" element={<Dashboard />} />
+//           <Route path="/inventory" element={Inventory} />
+//           <Route path="/shop-locator" element={ShopLocator} />
+//           <Route path="/agent-management" element={AgentManagement} />
+//           <Route path="/invoice" element={Invoice} />
+//           <Route path="/receipt" element={Receipt} />
+//         </Routes>
+//       </Container>
+//     </Router>
+//   );
+// };
+
+// export default App;
--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -19,7 +19,7 @@ class Dashboard extends React.Component {
   }
 
   componentDidMount() {
-    axios.get('http://localhost:5000/dashboard')
+    axios.get('http://localhost:5005/api/dashboard', {timeout: 5000})
       .then(res => {
         this.setState({
           totalSales: res.data.totalSales,
@@ -30,7 +30,7 @@ class Dashboard extends React.Component {
       })
       .catch(err => console.log(err));
       
-    axios.get('http://localhost:5000/products')
+    axios.get('http://localhost:5005/api/products', {timeout: 5000})
       .then(res => {
         this.setState({
           products: res.data,
@@ -101,6 +101,8 @@ export default Dashboard;
 // import { Card, CardBody, CardTitle } from 'reactstrap';
 // import axios from 'axios';
 // import './styles/dashboard.css';
+// import ProductList from './ProductList';
+// import ShoppingCart from './ShoppingCart';
 
 // class Dashboard extends React.Component {
 //   constructor(props) {
@@ -110,11 +112,13 @@ export default Dashboard;
 //       totalCustomers: 0,
 //       totalProducts: 0,
 //       isLoading: true,
+//       products: [],
+//       cartItems: [],
 //     };
 //   }
 
 //   componentDidMount() {
-//     axios.get('http://localhost:5000/dashboard')
+//     axios.get('http://localhost:5005/api/dashboard')
 //       .then(res => {
 //         this.setState({
 //           totalSales: res.data.totalSales,
@@ -124,40 +128,66 @@ export default Dashboard;
 //         });
 //       })
 //       .catch(err => console.log(err));
+      
+//     axios.get('http://localhost:5005/api/products')
+//       .then(res => {
+//         this.setState({
+//           products: res.data,
+//         });
+//       })
+//       .catch(err => console.log(err));
+//   }
+  
+//   addToCart = (product) => {
+//     this.setState({
+//       cartItems: [...this.state.cartItems, product],
+//     });
 //   }
 
 //   render() {
-//     const { totalSales, totalCustomers, totalProducts, isLoading } = this.state;
+//     const { totalSales, totalCustomers, totalProducts, isLoading, products, cartItems } = this.state;
 
 //     if (isLoading) {
 //       return <p>Loading...</p>;
 //     }
 
 //     return (
-//       <div className="row">
-//         <div className="col-md-4">
-//           <Card>
-//             <CardBody>
-//               <CardTitle>Total Sales</CardTitle>
-//               <h2>{totalSales}</h2>
-//             </CardBody>
-//           </Card>
+//       <div>
+//         <div className="row">
+//           <div className="col-md-4">
+//             <Card>
+//               <CardBody>
+//                 <CardTitle>Total Sales</CardTitle>
+//                 <h2>{totalSales}</h2>
+//               </CardBody>
+//             </Card>
+//           </div>
+//           <div className="col-md-4">
+//             <Card>
+//               <CardBody>
+//                 <CardTitle>Total Customers</CardTitle>
+//                 <h2>{totalCustomers}</h2>
+//               </CardBody>
+//             </Card>
+//           </div>
+//           <div className="col-md-4">
+//             <Card>
+//               <CardBody>
+//                 <CardTitle>Total Products</CardTitle>
+//                 <h2>{totalProducts}</h2>
+//               </CardBody>
+//             </Card>
+//           </div>
 //         </div>
-//         <div className="col-md-4">
-//           <Card>
-//             <CardBody>
-//               <CardTitle>Total Customers</CardTitle>
-//               <h2>{totalCustomers}</h2>
-//             </CardBody>
-//           </Card>
-//         </div>
-//         <div className="col-md-4">
-//           <Card>
-//             <CardBody>
-//               <CardTitle>Total Products</CardTitle>
-//               <h2>{totalProducts}</h2>
-//             </CardBody>
-//           </Card>
+//         <div className="row">
+//           <div className="col-md-8">
+//             <h2>Products</h2>
+//             <ProductList products={products} addToCart={this.addToCart} />
+//           </div>
+//           <div className="col-md-4">
+//             <h2>Shopping Cart</h2>
+//             <ShoppingCart cartItems={cartItems} />
+//           </div>
 //         </div>
 //       </div>
 //     );
@@ -165,3 +195,72 @@ export default Dashboard;
 // }
 
 // export default Dashboard;
+
+// // import React from 'react';
+// // import { Card, CardBody, CardTitle } from 'reactstrap';
+// // import axios from 'axios';
+// // import './styles/dashboard.css';
+
+// // class Dashboard extends React.Component {
+// //   constructor(props) {
+// //     super(props);
+// //     this.state = {
+// //       totalSales: 0,
+// //       totalCustomers: 0,
+// //       totalProducts: 0,
+// //       isLoading: true,
+// //     };
+// //   }
+
+// //   componentDidMount() {
+// //     axios.get('http://localhost:5000/dashboard')
+// //       .then(res => {
+// //         this.setState({
+// //           totalSales: res.data.totalSales,
+// //           totalCustomers: res.data.totalCustomers,
+// //           totalProducts: res.data.totalProducts,
+// //           isLoading: false,
+// //         });
+// //       })
+// //       .catch(err => console.log(err));
+// //   }
+
+// //   render() {
+// //     const { totalSales, totalCustomers, totalProducts, isLoading } = this.state;
+
+// //     if (isLoading) {
+// //       return <p>Loading...</p>;
+// //     }
+
+// //     return (
+// //       <div className="row">
+// //         <div className="col-md-4">
+// //           <Card>
+// //             <CardBody>
+// //               <CardTitle>Total Sales</CardTitle>
+// //               <h2>{totalSales}</h2>
+// //             </CardBody>
+// //           </Card>
+// //         </div>
+// //         <div className="col-md-4">
+// //           <Card>
+// //             <CardBody>
+// //               <CardTitle>Total Customers</CardTitle>
+// //               <h2>{totalCustomers}</h2>
+// //             </CardBody>
+// //           </Card>
+// //         </div>
+// //         <div className="col-md-4">
+// //           <Card>
+// //             <CardBody>
+// //               <CardTitle>Total Products</CardTitle>
+// //               <h2>{totalProducts}</h2>
+// //             </CardBody>
+// //           </Card>
+// //         </div>
+// //       </div>
+// //     );
+// //   }
+// // }
+
+// // export default Dashboard;
