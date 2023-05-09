ee5cc15c01e5f68e4700a27967ea8573912255f5
Diana Opiyo <dopiyo@gmail.com>
2023-05-09 08:48:18 +0300

    updated api connection

--- src/App.js
+++ src/App.js
@@ -1,8 +1,47 @@
 import React, { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import axios from 'axios';
+import './App.css';
 
-function App() {
-  const [dashboard, setDashboard] = useState({});
+class ErrorBoundary extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  static getDerivedStateFromError(error) {
+    return { hasError: true };
+  }
+
+  componentDidCatch(error, errorInfo) {
+    console.log('Error:', error);
+    console.log('Error Info:', errorInfo);
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return <h1>Something went wrong.</h1>;
+    }
+
+    return this.props.children;
+  }
+}
+
+function Home() {
+  return <h1>Welcome to the Online App</h1>;
+}
+
+function About() {
+  return (
+    <div>
+      <h1>About Us</h1>
+      <p>We are a company dedicated to providing quality online products to our customers.</p>
+    </div>
+  );
+}
+
+function Dashboard() {
+  const [dashboard, setDashboard] = useState(null);
   const [products, setProducts] = useState([]);
 
   useEffect(() => {
@@ -17,8 +56,12 @@ function App() {
 
   return (
     <div>
-      <h1>{dashboard.name}</h1>
-      <p>{dashboard.description}</p>
+      {dashboard &&
+        <>
+          <h1>{dashboard.name}</h1>
+          <p>{dashboard.description}</p>
+        </>
+      }
 
       <ul>
         {products.map(product => (
@@ -34,36 +77,376 @@ function App() {
   );
 }
 
+function App() {
+  return (
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About Us</Link>
+            </li>
+            <li>
+              <Link to="/dashboard">Dashboard</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <ErrorBoundary>
+          <Routes>
+            <Route path="/about">
+              <About />
+            </Route>
+            <Route path="/dashboard">
+              <Dashboard />
+            </Route>
+            <Route path="/">
+              <Home />
+            </Route>
+          </Routes>
+        </ErrorBoundary>
+
+        <footer>
+          <div>
+            <h4>Follow Us</h4>
+            <ul>
+              <li>
+                <a href="https://www.facebook.com/">Facebook</a>
+              </li>
+              <li>
+                <a href="https://www.twitter.com/">Twitter</a>
+              </li>
+              <li>
+                <a href="https://www.instagram.com/">Instagram</a>
+              </li>
+            </ul>
+          </div>
+
+          <div>
+            <h4>Contact Us</h4>
+            <ul>
+              <li>Email: contact@onlineapp.com</li>
+              <li>Phone: 555-555-5555</li>
+              <li>Address: 123 Main Street, Anytown USA</li>
+            </ul>
+          </div>
+        </footer>
+      </div>
+    </Router>
+  );
+}
+
 export default App;
-// import React from 'react';
+// import React, { useState, useEffect } from 'react';
+// import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+// import axios from 'axios';
 // import './App.css';
-// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-// import { Container } from 'reactstrap';
 
-// import Inventory from './components/Inventory';
-// import ShopLocator from './components/ShopLocator';
-// import AgentManagement from './components/AgentManagement';
-// import Invoice from './components/Invoice';
-// import Receipt from './components/Receipt';
-// import Dashboard from './components/Dashboard';
-// import Home from './components/Home'; // Import the Home component
 
-// const App = () => {
+// function Home() {
+//   return <h1>Welcome to the Online App</h1>;
+// }
+
+// function About() {
+//   return <h1>About Us</h1>;
+// }
+
+// function Dashboard() {
+//   const [dashboard, setDashboard] = useState(null);
+//   const [products, setProducts] = useState([]);
+
+//   useEffect(() => {
+//     axios.get('http://localhost:5005/api/dashboard')
+//       .then(res => setDashboard(res.data))
+//       .catch(err => console.log(err));
+
+//     axios.get('http://localhost:5005/api/products')
+//       .then(res => setProducts(res.data))
+//       .catch(err => console.log(err));
+//   }, []);
+
+//   return (
+//     <div>
+//       {dashboard &&
+//         <>
+//           <h1>{dashboard.name}</h1>
+//           <p>{dashboard.description}</p>
+//         </>
+//       }
+
+//       <ul>
+//         {products.map(product => (
+//           <li key={product._id}>
+//             <h2>{product.name}</h2>
+//             <p>{product.description}</p>
+//             <p>${product.price}</p>
+//             <p>{product.quantity} in stock</p>
+//           </li>
+//         ))}
+//       </ul>
+//     </div>
+//   );
+// }
+
+// function App() {
 //   return (
 //     <Router>
-//       <Container>
+//       <div>
+//         <nav>
+//           <ul>
+//             <li>
+//               <Link to="/">Home</Link>
+//             </li>
+//             <li>
+//               <Link to="/about">About Us</Link>
+//             </li>
+//             <li>
+//               <Link to="/dashboard">Dashboard</Link>
+//             </li>
+//           </ul>
+//         </nav>
+
 //         <Routes>
-//           <Route exact path="/" element={<Home />} /> {/* Add this new Route element */}
-//           <Route path="/dashboard" element={<Dashboard />} />
-//           <Route path="/inventory" element={Inventory} />
-//           <Route path="/shop-locator" element={ShopLocator} />
-//           <Route path="/agent-management" element={AgentManagement} />
-//           <Route path="/invoice" element={Invoice} />
-//           <Route path="/receipt" element={Receipt} />
+//           <Route path="/about">
+//             <About />
+//           </Route>
+//           <Route path="/dashboard">
+//             <Dashboard />
+//           </Route>
+//           <Route path="/">
+//             <Home />
+//           </Route>
 //         </Routes>
-//       </Container>
+
+//         <footer>
+//           <div>
+//             <h4>Follow Us</h4>
+//             <ul>
+//               <li>
+//                 <a href="https://www.facebook.com/">Facebook</a>
+//               </li>
+//               <li>
+//                 <a href="https://www.twitter.com/">Twitter</a>
+//               </li>
+//               <li>
+//                 <a href="https://www.instagram.com/">Instagram</a>
+//               </li>
+//             </ul>
+//           </div>
+
+//           <div>
+//             <h4>Contact Us</h4>
+//             <ul>
+//               <li>Email: contact@onlineapp.com</li>
+//               <li>Phone: 555-555-5555</li>
+//               <li>Address: 123 Main Street, Anytown USA</li>
+//             </ul>
+//           </div>
+//         </footer>
+//       </div>
 //     </Router>
 //   );
-// };
+// }
 
 // export default App;
+
+// import React, { useState, useEffect } from 'react';
+// import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+// import axios from 'axios';
+// import './App.css';
+
+// function Home() {
+//   return <h1>Welcome to the Online App</h1>;
+// }
+
+// function About() {
+//   return <h1>About Us</h1>;
+// }
+
+// function Dashboard() {
+//   const [dashboard, setDashboard] = useState(null);
+//   const [products, setProducts] = useState([]);
+
+//   useEffect(() => {
+//     axios.get('http://localhost:5005/api/dashboard')
+//       .then(res => setDashboard(res.data))
+//       .catch(err => console.log(err));
+
+//     axios.get('http://localhost:5005/api/products')
+//       .then(res => setProducts(res.data))
+//       .catch(err => console.log(err));
+//   }, []);
+
+//   return (
+//     <div>
+//       {dashboard &&
+//         <>
+//           <h1>{dashboard.name}</h1>
+//           <p>{dashboard.description}</p>
+//         </>
+//       }
+
+//       <ul>
+//         {products.map(product => (
+//           <li key={product._id}>
+//             <h2>{product.name}</h2>
+//             <p>{product.description}</p>
+//             <p>${product.price}</p>
+//             <p>{product.quantity} in stock</p>
+//           </li>
+//         ))}
+//       </ul>
+//     </div>
+//   );
+// }
+
+// function App() {
+//   return (
+//     <Router>
+//       <div>
+//         <nav>
+//           <ul>
+//             <li>
+//               <Link to="/">Home</Link>
+//             </li>
+//             <li>
+//               <Link to="/about">About Us</Link>
+//             </li>
+//             <li>
+//               <Link to="/dashboard">Dashboard</Link>
+//             </li>
+//           </ul>
+//         </nav>
+
+//         <Routes>
+//           <Route path="/about">
+//             <About />
+//           </Route>
+//           <Route path="/dashboard">
+//             <Dashboard />
+//           </Route>
+//           <Route path="/">
+//             <Home />
+//           </Route>
+//         </Routes>
+//       </div>
+//     </Router>
+//   );
+// }
+
+// export default App;
+// // import React, { useState, useEffect } from 'react';
+// // import axios from 'axios';
+// // import './App.css';
+
+// // function App() {
+// //   const [dashboard, setDashboard] = useState(null);
+// //   const [products, setProducts] = useState([]);
+
+// //   useEffect(() => {
+// //     axios.get('http://localhost:5005/api/dashboard')
+// //       .then(res => setDashboard(res.data))
+// //       .catch(err => console.log(err));
+
+// //     axios.get('http://localhost:5005/api/products')
+// //       .then(res => setProducts(res.data))
+// //       .catch(err => console.log(err));
+// //   }, []);
+
+// //   return (
+// //     <div>
+// //       {dashboard &&
+// //         <>
+// //           <h1>{dashboard.name}</h1>
+// //           <p>{dashboard.description}</p>
+// //         </>
+// //       }
+
+// //       <ul>
+// //         {products.map(product => (
+// //           <li key={product._id}>
+// //             <h2>{product.name}</h2>
+// //             <p>{product.description}</p>
+// //             <p>${product.price}</p>
+// //             <p>{product.quantity} in stock</p>
+// //           </li>
+// //         ))}
+// //       </ul>
+// //     </div>
+// //   );
+// // }
+
+// // export default App;
+
+// // // import React, { useState, useEffect } from 'react';
+// // // import axios from 'axios';
+
+// // // function App() {
+// // //   const [dashboard, setDashboard] = useState({});
+// // //   const [products, setProducts] = useState([]);
+
+// // //   useEffect(() => {
+// // //     axios.get('http://localhost:5005/api/dashboard')
+// // //       .then(res => setDashboard(res.data))
+// // //       .catch(err => console.log(err));
+
+// // //     axios.get('http://localhost:5005/api/products')
+// // //       .then(res => setProducts(res.data))
+// // //       .catch(err => console.log(err));
+// // //   }, []);
+
+// // //   return (
+// // //     <div>
+// // //       <h1>{dashboard.name}</h1>
+// // //       <p>{dashboard.description}</p>
+
+// // //       <ul>
+// // //         {products.map(product => (
+// // //           <li key={product._id}>
+// // //             <h2>{product.name}</h2>
+// // //             <p>{product.description}</p>
+// // //             <p>${product.price}</p>
+// // //             <p>{product.quantity} in stock</p>
+// // //           </li>
+// // //         ))}
+// // //       </ul>
+// // //     </div>
+// // //   );
+// // // }
+
+// // // export default App;
+// // // import React from 'react';
+// // // import './App.css';
+// // // import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+// // // import { Container } from 'reactstrap';
+
+// // // import Inventory from './components/Inventory';
+// // // import ShopLocator from './components/ShopLocator';
+// // // import AgentManagement from './components/AgentManagement';
+// // // import Invoice from './components/Invoice';
+// // // import Receipt from './components/Receipt';
+// // // import Dashboard from './components/Dashboard';
+// // // import Home from './components/Home'; // Import the Home component
+
+// // // const App = () => {
+// // //   return (
+// // //     <Router>
+// // //       <Container>
+// // //         <Routes>
+// // //           <Route exact path="/" element={<Home />} /> {/* Add this new Route element */}
+// // //           <Route path="/dashboard" element={<Dashboard />} />
+// // //           <Route path="/inventory" element={Inventory} />
+// // //           <Route path="/shop-locator" element={ShopLocator} />
+// // //           <Route path="/agent-management" element={AgentManagement} />
+// // //           <Route path="/invoice" element={Invoice} />
+// // //           <Route path="/receipt" element={Receipt} />
+// // //         </Routes>
+// // //       </Container>
+// // //     </Router>
+// // //   );
+// // // };
+
+// // // export default App;
--- webpack.config.js
+++ webpack.config.js
@@ -1,42 +1,42 @@
-// const path = require('path');
+const path = require('path');
 
-// module.exports = {
-//   mode: 'development',
-//   entry: './src/index.js',
-//   output: {
-//     path: path.resolve(__dirname, 'dist'),
-//     filename: 'bundle.js'
-//   },
-//   devServer: {
-//     port: 3000,
-//     static: {
-//       directory: path.join(__dirname, 'public'),
-//     },
-//     devMiddleware: {
-//       index: true,
-//       publicPath: '/',
-//       writeToDisk: true,
-//     },
-//     proxy: {
-//       '/api': {
-//         target: 'http://localhost:5000',
-//         pathRewrite: { '^/api': '' },
-//       },
-//     },
-//     historyApiFallback: true,
-//     open: true,
-//   },
-//   module: {
-//     rules: [
-//       {
-//         test: /\.js$/,
-//         exclude: /node_modules/,
-//         use: ['babel-loader']
-//       },
-//       {
-//         test: /\.css$/,
-//         use: ['style-loader', 'css-loader']
-//       }
-//     ]
-//   }
-// }
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  devServer: {
+    port: 3000,
+    static: {
+      directory: path.join(__dirname, 'public'),
+    },
+    devMiddleware: {
+      index: true,
+      publicPath: '/',
+      writeToDisk: true,
+    },
+    proxy: {
+      '/api': {
+        target: 'http://localhost:5005',
+        pathRewrite: { '^/api': '' },
+      },
+    },
+    historyApiFallback: true,
+    open: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
+  }
+}
