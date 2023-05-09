3eeb97e1c9bbf4c4a5f4cc79a84141a3fe94095d
Pius <mutundu.nguma@gmail.com>
2023-05-09 12:42:11 +0300

    Worked on Top navbar

---
+++ src/components/Faqs.js
@@ -0,0 +1,16 @@
+import React from 'react';
+//import { Link } from 'react-router-dom';
+import './styles/faqs.css';
+
+const Faqs = () => {
+  return (
+    <div className="FAQs">
+      <div className="banner">
+        <h1>What you should know</h1>
+        <p></p>
+      </div>
+    </div>
+  );
+};
+
+export default Faqs;
