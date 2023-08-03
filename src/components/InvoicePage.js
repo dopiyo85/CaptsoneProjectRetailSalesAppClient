325a2db96b478d51c8d48b470a22e64fec5d5d34
dmuthui <vyda2002us@gmail.com>
2023-08-03 09:26:32 +0300

    Adds the Components and the styles to the project

--- src/components/InvoicePage.js
+++ src/components/InvoicePage.js
@@ -1,16 +1,41 @@
 import React from 'react';
+import { Page, Text, View, Document, StyleSheet, PDFDownloadLink } from '@react-pdf/renderer';
+import InvoicePDF from './InvoicePDF';
 
-const InvoicePage = ({ invoiceId, totalPrice }) => {
-  // Display invoice details and payment button
+const styles = StyleSheet.create({
+  // Define your styles for the PDF (same as in InvoicePDF.js)
+});
+
+const InvoicePage = ({ invoiceId, totalPrice, cartItems }) => {
   return (
-    <div>
-      <h2>Invoice</h2>
-      {/* Display invoice details */}
+    <div className="invoice-page" style={{ textAlign: 'center', maxWidth: '400px', margin: '0 auto' }}>
+      <h2 style={{ fontSize: '24px', marginBottom: '20px', color: 'orange', textShadow: '1px 1px 2px rgba(0, 0, 0, 0.2)', fontWeight: 'bold' }}>INVOICE</h2>
       <p>Invoice ID: {invoiceId}</p>
-      <p>Total Price: Ksh. {totalPrice}</p>
+      <table style={{ width: '100%', borderCollapse: 'collapse', border: '1px solid #ddd', textAlign: 'left' }}>
+        <thead>
+          <tr>
+            <th style={{ border: '1px solid #ddd', padding: '8px' }}>Product</th>
+            <th style={{ border: '1px solid #ddd', padding: '8px' }}>Price</th>
+          </tr>
+        </thead>
+        <tbody>
+          {cartItems.map((item) => (
+            <tr key={item.id}>
+              <td style={{ border: '1px solid #ddd', padding: '8px' }}>{item.name}</td>
+              <td style={{ border: '1px solid #ddd', padding: '8px' }}>Kshs.{item.price}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      <p style={{ marginTop: '10px' }}>Total Price: Ksh. {totalPrice}</p>
 
-      {/* Display payment button */}
-      <button>Make Payment</button>
+      {/* Link to download the PDF invoice */}
+      <PDFDownloadLink
+        document={<InvoicePDF invoiceId={invoiceId} totalPrice={totalPrice} cartItems={cartItems} />}
+        fileName={`Invoice_${invoiceId}.pdf`}
+      >
+        {({ loading }) => (loading ? 'Loading...' : 'Download Invoice')}
+      </PDFDownloadLink>
     </div>
   );
 };
